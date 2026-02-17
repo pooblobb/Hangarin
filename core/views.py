000acb55b6e90b03cec218692643ba7d66e9d010
pooblobb <paulbryandelacruz.3429@gmail.com>
2026-02-17 19:39:35 +0000

    Changes made in pythonanywhere

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+def index(request):
+    # Get the status from the URL query parameters (if any)
+    status_filter = request.GET.get('status')
+
+    # Filter tasks based on the parameter, otherwise get all
+    if status_filter:
+        tasks = Task.objects.filter(status=status_filter)
+    else:
+        tasks = Task.objects.all()
+
+    context = {
+        'tasks': tasks,
+        'current_status': status_filter # Pass status to template for highlighting
+    }
+    return render(request, 'core/index.html', context)
